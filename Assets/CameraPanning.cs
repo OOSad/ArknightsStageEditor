2a4d3c5e5ed69a1a7c6c769410bb76e9c1c7436f
OOSad <77910065+OOSad@users.noreply.github.com>
2021-05-18 16:55:56 -0300

    Added script for camera panning left right by holding right mouse button down

---
+++ Assets/CameraPanning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraPanning : MonoBehaviour
+{
+    private Mouse mouse;
+
+    private void Update()
+    {
+        mouse = Mouse.current;
+
+        if (mouse.rightButton.isPressed)
+        {
+            this.transform.Translate((mouse.delta.x.ReadValue() * -1) * Time.deltaTime, 0, 0);
+        }
+    }
+}
