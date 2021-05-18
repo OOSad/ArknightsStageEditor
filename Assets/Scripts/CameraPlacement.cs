908b3012fffbaccaea6c46dabbbd22d1e3397bdd
OOSad <77910065+OOSad@users.noreply.github.com>
2021-05-18 13:31:43 -0300

    Changed the way the camera calculates where to place itself based on stage width/height
    Also deleted useless cursor raycasting script

--- Assets/Scripts/CameraPlacement.cs
+++ Assets/Scripts/CameraPlacement.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class CameraPlacement : MonoBehaviour
 {
-    private void Update()
-    {
-        int stageWidthEditor = GameObject.FindGameObjectWithTag("StageGenerator").GetComponent<StageGenerator>().stageWidthEditor;
+    public StageGenerator stageGeneratorScene;
 
-        this.transform.position = new Vector3 (stageWidthEditor / 2, 7, -0.6f);
+    private void Start()
+    {
+        this.transform.position = new Vector3(stageGeneratorScene.stageWidthEditor / 2, 8, -1.2f);
+    }
+
+    public void UpdateCameraPosition()
+    {
+        float camera_x = stageGeneratorScene.stageWidthEditor / 2;
+        float camera_y = stageGeneratorScene.stageHeightEditor + 3;
+
+        if (stageGeneratorScene.stageWidthEditor % 2 == 0)
+        {
+            this.transform.position = new Vector3(camera_x, camera_y, -1.2f);
+        }
+
+        else
+        {
+            this.transform.position = new Vector3(camera_x + 0.5f, camera_y, -1.2f);
+        }
     }
 }
