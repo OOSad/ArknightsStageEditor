2f49646741b95f01745d4c87bc644013a4b8dde9
OOSad <77910065+OOSad@users.noreply.github.com>
2021-05-25 20:34:56 -0300

    Changed BallistaBoltShooter class name to BallistaBolt
    Also added a ShootBoltForward function to make code more readable

--- Assets/Scripts/Special Block Functionality/BallistaBolt.cs
+++ Assets/Scripts/Special Block Functionality/BallistaBolt.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BallistaBoltShooter : MonoBehaviour
+public class BallistaBolt : MonoBehaviour
 {
     private void Start()
     {
-        Invoke("DestroyBolt", 3.0f);
+        Invoke("DestroyBolt", 1.0f);
     }
 
     void Update()
     {
-        this.transform.localPosition += new Vector3(0, 0, 0.5f);             
+        ShootBoltForward();
     }
 
     private void DestroyBolt()
     {
         Destroy(this.gameObject);
+    }
+
+    private void ShootBoltForward()
+    {
+        this.transform.localPosition += new Vector3(0, 0, 0.5f);
     }
 }
