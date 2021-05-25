78f27de1ee260f885f7d69c642e4b9cdf65cb8ac
OOSad <77910065+OOSad@users.noreply.github.com>
2021-05-25 20:31:29 -0300

    Changed classes in charge of altar tiles and their effects a bit
    Mostly naming changes

--- Assets/Scripts/Special Block Functionality/AltarAoE.cs
+++ Assets/Scripts/Special Block Functionality/AltarAoE.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AltarEffect : MonoBehaviour
+public class AltarAoE : MonoBehaviour
 {
     private void Start()
     {
---
+++ Assets/Scripts/Special Block Functionality/AltarTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarTile : MonoBehaviour
+{
+    public GameObject altarAoE;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnAltarAoE", 10.0f, 10.0f);
+    }
+
+    private void SpawnAltarAoE()
+    {
+        Instantiate(altarAoE, this.gameObject.transform);
+    }
+}
